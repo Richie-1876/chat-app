2e9393f11254491cd3e4f21c70b46d1e74d84a38
Richard Wright <richardwright@Richards-MacBook-Pro-1.local>
2021-01-12 17:51:26 +0000

    First commit - backend with auth set up and front end structure set up with create react app

---
+++ chat_backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const config = require('./config/app')
+
+const router = require('./router')
+
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.use(router)
+
+const PORT = config.appPort
+
+app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`)
+})
+
